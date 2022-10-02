50f1ba58916a0d4daae2d944f7936a51ac04c9b7
Andrei Cabungcal <40614905+RoyalCookieX@users.noreply.github.com>
2022-10-01 21:36:35 -0500

    Enemies can fire at the player

--- Assets/LudumDare51/Scripts/Character/AIController.cs
+++ Assets/LudumDare51/Scripts/Character/AIController.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class AIController : MonoBehaviour
 {
     [Header("Components")]
+    [SerializeField] private ProjectileLauncher _launcher;
     [SerializeField] private Rotator _rotator;
 
     public void SetTarget(GameObject target)
     {
         _rotator.SetFollowTransform(target.transform);
+    }
+
+    private void Update()
+    {
+        _launcher.Launch();
     }
 }
